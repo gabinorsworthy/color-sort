3fa5228359729bdacb2b2a22e1734a135cac28bf
gabinorsworthy <gabinorsworthy@tamu.edu>
2020-07-30 00:19:53 -0500

    completed bubble sort animation

--- src/sortingAlgorithms/bubbleSort.js
+++ src/sortingAlgorithms/bubbleSort.js
@@ -5,6 +5,9 @@ export function getBubbleSortAnimations(array) {
         let swapped = false;
         
         for (let j = 0; j < (array.length - i - 1); j++) {
+            animations.push([j, j + 1]);
+            animations.push([j, j + 1]);
+
             if (array[j] > array[j + 1]) {
                 let temp = array[j];
                 array[j] = array[j + 1];
@@ -13,12 +16,19 @@ export function getBubbleSortAnimations(array) {
 
                 animations.push([j, j+1]);
             }
+            else{
+                animations.push([j + 1, j + 1]);
+            }
         }
 
+        /* removed break for animation purpose
+            removing the break allows for all color heights to get adjusted to a uniform height at the end
         if (!swapped){
             break;
         }
+        */
     }
+    animations.push([0,0]);
     
     return animations;
 }
