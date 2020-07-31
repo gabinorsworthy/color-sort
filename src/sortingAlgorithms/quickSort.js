ef7e8d73b51f9ef5201d04867c37028c8c2f22dd
gabinorsworthy <gabinorsworthy@tamu.edu>
2020-07-31 00:08:57 -0500

    quicksort animation

--- src/sortingAlgorithms/quickSort.js
+++ src/sortingAlgorithms/quickSort.js
@@ -1,56 +1,88 @@
 export function getQuickSortAnimations(array) {
     //console.log(array);
+    let animations = [];
        
-    quickSort(array, 0, array.length - 1);
+    quickSort(array, 0, array.length - 1, animations);
 
-    return array;
+    //console.log(array);
+    console.log(animations);
+    return animations;
 
 }
 
 
-function quickSort(array, low, high) {
+function quickSort(array, low, high, animations) {
     //console.log(low, high);
     //console.log(array);
 
     if (low < high) {
-        var partitionIdx = partition(array, low, high);
+        var partitionIdx = partition(array, low, high, animations);
 
-        quickSort(array, low, partitionIdx - 1);
-        quickSort(array, partitionIdx + 1, high);
+        quickSort(array, low, partitionIdx - 1, animations);
+        quickSort(array, partitionIdx + 1, high, animations);
+    }
+    else if (low < array.length) {
+        animations.push(['changeHeightFinal', low, low]);  
     }
     
 }
 
 
-function partition(array, low, high) {
-    var pivot = array[low];
-    var i = low + 1;
-    var j = high;
+function partition(array, low, high, animations) {
+    var pivot = array[high];
+    var i = low;
+    var j = high - 1;
 
-    while (i <= j) {
-        //console.log('i:', i);
-        //console.log('j:', j);
+    animations.push(['piv', high, high]);
+    animations.push(['incHeight', i, j]);
+
+    while (true) {
         
-        while (array[i] < pivot) { i++; }
+        while (array[i] < pivot) {
+            i++;
+            if (!(i > j)) {
+                animations.push(['changeHeight', i - 1, i, 'i while']);
+            }
+        }
 
-        while (array[j] > pivot) { j--; }
+        while ((j > 0) && (array[j] > pivot)) {
+            j--;
+            if (!(i > j)){
+                animations.push(['changeHeight', j + 1, j, 'j while']);
+            } 
+        }
 
-        if (i <= j ) {
+        if (i >= j) {
+            break;
+        }
+        else {
             var temp = array[i];
             array[i] = array[j];
             array[j] = temp;
+            animations.push(['changeColor', i, j]);
 
             i++;
             j--;
+            if (!(i > j)) {
+                animations.push(['changeHeight', i - 1, i, 'change color']);
+                animations.push(['changeHeight', j + 1, j, 'change color']);
+            }   
         }
     }
-
-    var partitionIdx = Math.min(i, j);
-
-    array[low] = array[partitionIdx];
+    
+    var partitionIdx = i;
+    
+    
+    if ((j >= low) && (i != high)) {
+        animations.push(['changeHeight', j, i]);
+    }
+    
+    array[high] = array[partitionIdx];
     array[partitionIdx] = pivot;
 
-    //console.log(array);
+    animations.push(['matchPivotHeight', high, partitionIdx]);
+    animations.push(['swapPivot', high, partitionIdx]);
+    animations.push(['changeHeightFinal', high, partitionIdx]);
 
     return partitionIdx;
 }
